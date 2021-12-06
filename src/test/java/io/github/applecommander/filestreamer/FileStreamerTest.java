0755d302e6acf90191ea961f1fa1d0f9952d6d76
Rob Greene <robgreene@gmail.com>
2021-12-05 21:58:10 -0600

    Switching to JUnit 5.

--- src/test/java/io/github/applecommander/filestreamer/FileStreamerTest.java
+++ src/test/java/io/github/applecommander/filestreamer/FileStreamerTest.java
@@ -1,13 +1,13 @@
 package io.github.applecommander.filestreamer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.webcodepro.applecommander.storage.DiskUnrecognizedException;
 
--- src/test/java/io/github/applecommander/filestreamer/FileTupleTest.java
+++ src/test/java/io/github/applecommander/filestreamer/FileTupleTest.java
@@ -1,11 +1,11 @@
 package io.github.applecommander.filestreamer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.webcodepro.applecommander.storage.Disk;
 import com.webcodepro.applecommander.storage.DiskException;
